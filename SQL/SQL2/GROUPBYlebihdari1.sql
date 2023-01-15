6b2258edf742bce784a57550732ee2085291d997
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-01-15 21:51:44 +0700

    Create GROUPBYlebihdari1.sql

---
+++ SQL/SQL2/GROUPBYlebihdari1.sql
@@ -0,0 +1,14 @@
+-- dapatkan total harga purchases berdasarkan purchased_at dan character_name
+SELECT SUM(price), purchased_at, character_name 
+FROM purchases
+GROUP BY purchased_at, character_name;
+
+
+/*
+dapatkan total berapa kali purchases terjadi berdasarkan
+purchased_at and character_name
+*/
+
+SELECT COUNT(*), purchased_at, character_name 
+FROM purchases
+GROUP BY purchased_at, character_name;
