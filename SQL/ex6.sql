74fbc802e044ef2d6af565ba4dd100e4132b511c
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-01-15 12:25:02 +0700

    Delete ex6.sql

--- SQL/ex6.sql
+++
@@ -1,9 +0,0 @@
-/*
-dibawah "FROM purchases" tambahkan code untuk mengambil maksimum 5 baris
-dengan urutan terbesar dari kolom "price"
-*/
-
-SELECT *
-FROM purchases
-ORDER BY price DESC
-LIMIT 5;
