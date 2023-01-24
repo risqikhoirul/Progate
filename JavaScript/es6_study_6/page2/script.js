b52bfac1b95c3332ce990ab30fda20ddacd8f928
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-01-24 15:47:41 +0700

    Add files via upload

---
+++ JavaScript/es6_study_6/page2/script.js
@@ -0,0 +1,4 @@
+const characters = ["Ninja Ken", "Baby Ben", "Guru Domba", "Birdie"];
+
+// Cetak semua element di dalam array characters dengan menggunakan method forEach
+characters.forEach((character) => {console.log(character)});
