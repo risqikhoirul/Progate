2ae7b85a80a7018e378089e1db1a774de6d7b609
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-01-24 15:39:36 +0700

    Add files via upload

---
+++ JavaScript/es6_study_1/page15/script.js
@@ -0,0 +1,8 @@
+const age = 17;
+
+// Ketika kondisi tidak terpenuhi, cetak "Saya berusia di bawah 18 tahun"
+if (age >= 18) {
+  console.log("Saya berusia diatas 18 tahun");
+} else {
+  console.log("Saya berusia di bawah 18 tahun");
+}
